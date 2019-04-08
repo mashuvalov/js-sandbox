b340642d04c289eeb1a9a4874c2f468cca54b661
mashuvalov <m.a.shuvalov@gmail.com>
2019-04-08 22:53:45 +0400

    Update deploy.php

--- deploy.php
+++ deploy.php
@@ -3,5 +3,6 @@ ini_set('error_reporting', E_ALL);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 
-system('git pull');
+$conn = ssh2_connect('ftp-srv35573.ht-systems.ru', 22);
+ssh2_auth_password($conn, 'srv35573', 'z&KGP5a$h#6x');
 ?>
