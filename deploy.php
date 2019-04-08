3bd573ac6b9353987b71ce4ba0c8bc3004797676
mashuvalov <m.a.shuvalov@gmail.com>
2019-04-08 23:18:13 +0400

    Update deploy.php

--- deploy.php
+++ deploy.php
@@ -5,7 +5,7 @@ ini_set('display_startup_errors', 1);
 
 include 'Net/SSH2.php';
  
-     $ssh = new Net_SSH2('ftp-srv35573.ht-systems.ru');
+     $ssh = new Net_SSH2('ssh-srv35573.ht-systems.ru');
      if (!$ssh->login('srv35573', 'z&KGP5a$h#6x')) {
          exit('Login Failed');
      }
