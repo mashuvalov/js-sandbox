0cd4b43eb5e5a01014d7a91e152013ee95e355dd
mashuvalov <m.a.shuvalov@gmail.com>
2019-04-08 23:34:41 +0400

    Update deploy.php

--- deploy.php
+++ deploy.php
@@ -5,11 +5,14 @@ ini_set('display_startup_errors', 1);
 
 include 'Net/SSH2.php';
  
-     $ssh = new Net_SSH2('ssh-srv35573.ht-systems.ru');
-     if (!$ssh->login('srv35573', 'z&KGP5a$h#6x')) {
-         exit('Login Failed');
-     }
- 
-     echo $ssh->exec('pwd');
-     echo $ssh->exec('ls -la');
+$ssh = new Net_SSH2('ssh-srv35573.ht-systems.ru');
+if (!$ssh->login('srv35573', 'z&KGP5a$h#6x')) {
+    exit('Login Failed');
+}
+
+echo $ssh->exec('pwd');
+echo "<br>";
+echo $ssh->exec('cd sandbox.shvlv.ru/');
+echo $ssh->exec('pwd');
+//echo $ssh->exec('ls -la');
 ?>
