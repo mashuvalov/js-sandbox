2169585c09f0ea43a07508db6813778e000de9ee
mashuvalov <m.a.shuvalov@gmail.com>
2019-04-08 23:09:33 +0400

    Update deploy.php

--- deploy.php
+++ deploy.php
@@ -3,6 +3,13 @@ ini_set('error_reporting', E_ALL);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 
-$conn = ssh2_connect('ftp-srv35573.ht-systems.ru', 22);
-ssh2_auth_password($conn, 'srv35573', 'z&KGP5a$h#6x');
+include 'Net/SSH2.php';
+ 
+     $ssh = new Net_SSH2('ftp-srv35573.ht-systems.ru');
+     if (!$ssh->login('srv35573', 'z&KGP5a$h#6x')) {
+         exit('Login Failed');
+     }
+ 
+     echo $ssh->exec('pwd');
+     echo $ssh->exec('ls -la');
 ?>
