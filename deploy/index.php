b93b2972bf293e3825e32a091afc68f9d519dc56
mashuvalov <m.a.shuvalov@gmail.com>
2019-04-10 23:23:41 +0400

    Подставляется имя домена теперь само

--- deploy/index.php
+++ deploy/index.php
@@ -6,5 +6,7 @@ if (!$ssh->login('srv35573', 'z&KGP5a$h#6x')) {
     exit('Login Failed');
 }
 
-echo $ssh->exec('cd sandbox.shvlv.ru; git pull');
+$comand = 'cd ' + $_SERVER['SERVER_NAME'] + '; git pull';
+
+echo $ssh->exec(comand);
 ?>
