2f3146efd909aec8c7eb6ba7f1abca81d0fae09b
mashuvalov <m.a.shuvalov@gmail.com>
2019-04-10 23:23:52 +0400

    Revert "Подставляется имя домена теперь само"
    This reverts commit b93b2972bf293e3825e32a091afc68f9d519dc56.

--- deploy/index.php
+++ deploy/index.php
@@ -6,7 +6,5 @@ if (!$ssh->login('srv35573', 'z&KGP5a$h#6x')) {
     exit('Login Failed');
 }
 
-$comand = 'cd ' + $_SERVER['SERVER_NAME'] + '; git pull';
-
-echo $ssh->exec(comand);
+echo $ssh->exec('cd sandbox.shvlv.ru; git pull');
 ?>
