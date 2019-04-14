0595fbc9ea04363719aef9da41214e175090633a
mashuvalov <m.a.shuvalov@gmail.com>
2019-04-14 21:41:07 +0400

    Обновил деплой

--- deploy/index.php
+++ deploy/index.php
@@ -6,7 +6,7 @@ if (!$ssh->login('srv35573', 'z&KGP5a$h#6x')) {
     exit('Login Failed');
 }
 
-$comand = 'cd '.$_SERVER['SERVER_NAME'].'; git pull';
+$comand = 'cd '.$_SERVER['SERVER_NAME'].'; git pull; git add -A; git commit -m "Commited from hts.ru - '.$_SERVER['SERVER_NAME'].' - '.date("m.d.y H:i:s").'"; git push';
 
 echo $ssh->exec($comand);
 ?>
